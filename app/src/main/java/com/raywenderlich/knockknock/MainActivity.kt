22f7c0ab5fd610b40611e37f032a2070a39c64cd
Dean Djermanovic <dean.djermanovic@five.agency>
2019-01-07 22:07:45 +0100

    Add TAG

--- app/src/main/java/com/raywenderlich/knockknock/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/knockknock/MainActivity.kt
@@ -27,6 +27,10 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+  companion object {
+      const val TAG = "MainActivity"
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     initialize()
